b81b3c0ad2b5f93fbfa38c6dbd5c57b7b2308a29
petteri.stenius@ubisecure.com <petteri.stenius@ubisecure.com>
2020-11-15 23:19:49 +0200

    updates

--- Startup.cs
+++ Startup.cs
@@ -5,38 +5,11 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Http;
 using System;
+using AspNetCoreSample.Utils;
 
 namespace AspNetCoreSample
 {
-    /// <summary>
-    /// Workaround for https://github.com/dotnet/aspnetcore/issues/19939
-    /// Applies only when app is hosted on http://localhost
-    /// </summary>
-    public class CookieBuilderFilter : CookieBuilder
-    {
-        public CookieBuilderFilter(CookieBuilder cookieBuilder)
-        {
-            Domain = cookieBuilder.Domain;
-            Expiration = cookieBuilder.Expiration;
-            HttpOnly = cookieBuilder.HttpOnly;
-            IsEssential = cookieBuilder.IsEssential;
-            MaxAge = cookieBuilder.MaxAge;
-            Name = cookieBuilder.Name;
-            Path = cookieBuilder.Path;
-            SameSite = cookieBuilder.SameSite;
-            SecurePolicy = cookieBuilder.SecurePolicy;
-        }
-        public override CookieOptions Build(HttpContext context, DateTimeOffset expiresFrom)
-        {
-            if (SameSite == SameSiteMode.None && !context.Request.IsHttps && SecurePolicy != CookieSecurePolicy.Always)
-            {
-                SameSite = SameSiteMode.Unspecified;
-            }
-            return base.Build(context, expiresFrom);
-        }
-    }
     public class Startup
     {
         public Startup(IConfiguration configuration)
---
+++ Utils/CookieBuilderFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace AspNetCoreSample.Utils
+{
+    /// <summary>
+    /// Workaround for https://github.com/dotnet/aspnetcore/issues/19939
+    /// Applies only when app is hosted on http://localhost
+    /// </summary>
+    public class CookieBuilderFilter : CookieBuilder
+    {
+        public CookieBuilderFilter(CookieBuilder cookieBuilder)
+        {
+            Domain = cookieBuilder.Domain;
+            Expiration = cookieBuilder.Expiration;
+            HttpOnly = cookieBuilder.HttpOnly;
+            IsEssential = cookieBuilder.IsEssential;
+            MaxAge = cookieBuilder.MaxAge;
+            Name = cookieBuilder.Name;
+            Path = cookieBuilder.Path;
+            SameSite = cookieBuilder.SameSite;
+            SecurePolicy = cookieBuilder.SecurePolicy;
+        }
+        public override CookieOptions Build(HttpContext context, DateTimeOffset expiresFrom)
+        {
+            if (SameSite == SameSiteMode.None && !context.Request.IsHttps && SecurePolicy != CookieSecurePolicy.Always)
+            {
+                SameSite = SameSiteMode.Unspecified;
+            }
+            return base.Build(context, expiresFrom);
+        }
+    }
+}
